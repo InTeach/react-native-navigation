7b68fd5b0a4df157ded27cf75a895a47690fb00c
Quentin Valmori <quentin.valmori@gmail.com>
2017-10-09 11:58:33 +0200

    Update NavigationReactPackage.java

--- android/app/src/main/java/com/reactnativenavigation/bridge/NavigationReactPackage.java
+++ android/app/src/main/java/com/reactnativenavigation/bridge/NavigationReactPackage.java
@@ -20,7 +20,6 @@ public class NavigationReactPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
